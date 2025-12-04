5c67b581ed170e334e3f9624aec2e9d625256789
Jcalixte24 <japhetndri15@gmail.com>
2025-12-04 14:28:43 +0100

    th test with test ²# Please enter the commit message for your changes. Lines starting

--- consumer.py
+++ consumer.py
@@ -4,8 +4,7 @@ import time
 import os
 from database import create_tables, insert_ticket, insert_article
 
-# 1. On prépare la base de données
-print("⏳ Démarrage du Consumer...")
+
 create_tables()
 
 # 2. On se connecte à 
@@ -15,17 +14,17 @@ while not consumer:
         consumer = KafkaConsumer(
             'tickets_caisse',
             bootstrap_servers=[os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')],
-            auto_offset_reset='earliest',       # Lit les vieux messages ratés
-            group_id='groupe_sqlite',           # ID du groupe de travailleurs
+            auto_offset_reset='earliest',       
+            group_id='groupe_sqlite',           
             value_deserializer=lambda x: json.loads(x.decode('utf-8')) # Décode le JSON
         )
-        print("✅ Connecté à Kafka !")
+        print("Connecté à Kafka !")
     except:
-        print("⚠️ En attente de Kafka...")
+        print("En attente de Kafka...")
         time.sleep(2)
 
 # 3. La boucle de travail infinie
-print("🎧 Prêt à travailler...")
+
 
 for message in consumer:
     ticket = message.value
@@ -49,4 +48,4 @@ for message in consumer:
                 art['prix']
             )
     
-    print("💾 Ticket sauvegardé en base de données.")
+    print("Succès")
